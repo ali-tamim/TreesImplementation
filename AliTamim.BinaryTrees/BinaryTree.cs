1fc4f92d7e79abe3240379a59502a060c22c88c7
ali tamim <amoh331@gmail.com>
2022-08-20 15:45:03 +0300

    add BinaryTree with node

---
+++ AliTamim.BinaryTrees/BinaryTree.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AliTamim.BinaryTrees
+{
+    public class BinaryTree<T> : IEnumerable<T> where T : IComparable<T>
+    {
+        #region Add 
+        public void Add(T value)
+        {
+            throw new NotImplementedException();
+        }
+        public void AddTo(BinaryTreeNode<T> node, T value)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+        #region Find
+        #endregion
+
+        #region remove
+        #endregion
+
+        #region IEnumerable
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
---
+++ AliTamim.BinaryTrees/BinaryTreeNode.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AliTamim.BinaryTrees
+{
+    public class BinaryTreeNode<TNode> : IComparable<TNode> where TNode : IComparable<TNode>
+    {
+        public BinaryTreeNode<TNode>? Left { get; set; }
+        public BinaryTreeNode<TNode>? Right { get; set; }
+
+        public TNode Value { get; private set; }
+
+        public BinaryTreeNode(TNode value)
+        {
+            Value = value;
+        }
+
+        public int CompareTo(TNode? other)
+        {
+            return Value.CompareTo(other);
+        }
+    }
+}
