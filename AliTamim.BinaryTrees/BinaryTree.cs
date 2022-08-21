e98000411f1273870a2ad58c5be3b7cb9795c8e3
ali tamim <amoh331@gmail.com>
2022-08-21 12:33:34 +0300

    add fint and remove logic

--- AliTamim.BinaryTrees/BinaryTree.cs
+++ AliTamim.BinaryTrees/BinaryTree.cs
@@ -50,9 +50,119 @@ namespace AliTamim.BinaryTrees
         #endregion
 
         #region Find
+        public bool Contains(T value)
+        {
+            BinaryTreeNode<T> parent;
+            return FindWithParent(value, out parent) != null;
+        }
+
+        private BinaryTreeNode<T> FindWithParent(T value, out BinaryTreeNode<T> parent)
+        {
+            BinaryTreeNode<T> current = _head;
+            parent = null;
+
+            while (current != null)
+            {
+                int result = current.CompareTo(value);
+                if (result > 0)
+                {
+                    parent = current;
+                    current = current.Left;
+                } else if (result < 0)
+                {
+                    parent = current;
+                    current = current.Right;
+                } else
+                {
+                    break;
+                }
+            }
+
+            return current;
+        }
         #endregion
 
         #region remove
+        public bool Remove(T value)
+        {
+            BinaryTreeNode<T> current, parent;
+
+            current = FindWithParent(value, out parent);
+
+            if (current == null)
+            {
+                return false;
+            }
+            _count--;
+
+            if (current.Right == null)
+            {
+                if (parent == null)
+                {
+                    _head = current.Left;
+                } else
+                {
+                    int result = parent.CompareTo(current.Value);
+                    if (result > 0)
+                    {
+                        parent.Left = current.Left;
+                    } else if (result < 0)
+                    {
+                        parent.Right = current.Left;
+                    }
+                }
+            } else if (current.Right.Left == null)
+            {
+                current.Right.Left = current.Left; // reconncted
+                if (parent == null)
+                {
+                    _head = current.Left;
+                } else
+                {
+                    int result = parent.CompareTo(current.Value);
+                    if (result > 0)
+                    {
+                        parent.Left = current.Right;
+                    }
+                    else if (result < 0)
+                    {
+                        parent.Right = current.Right;
+                    }
+                }
+            } else
+            {
+                BinaryTreeNode<T> leftmost = current.Right.Left;
+                BinaryTreeNode<T> leftmostParent = current.Right.Left;
+
+                while (leftmost.Left != null)
+                {
+                    leftmostParent = leftmost;
+                    leftmost = leftmost.Left;
+                }
+
+                leftmostParent.Left = leftmost.Right;
+
+                leftmost.Left = current.Left;
+                leftmost.Right = current.Right;
+
+                if (parent == null)
+                {
+                    _head = leftmost;
+                } else
+                {
+                    int result = parent.CompareTo(current.Value);
+                    if (result > 0)
+                    {
+                        parent.Left = leftmost;
+                    }
+                    else if (result < 0)
+                    {
+                        parent.Right = leftmost;
+                    }
+                }
+            }
+            return true;
+        }
         #endregion
 
         #region IEnumerable
